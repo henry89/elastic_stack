977bc52f8919b09494f90c7dd8560643f8085914
Henry <henryakinola59@gmail.com>
2020-03-07 15:03:47 +0000

    logstash cookbook created, install and configuration working

--- chef/cookbooks/elasticsearch/recipes/elasticsearch_build.rb
+++ chef/cookbooks/elasticsearch/recipes/elasticsearch_build.rb
@@ -2,7 +2,10 @@
 require 'mixlib/shellout'
 
 
-#Install elasticsearch from the RPM repo
+=begin    
+Install elasticsearch from the RPM repo, will run check to see if elastic 7.6 version is installed 
+before applying resources    
+=end
 
 # Import the Elasticsearch PGP Key
 execute 'download_and_install_elasticsearch_public_key' do
@@ -17,34 +20,35 @@ template '/etc/yum.repos.d/elasticsearch.repo' do
     action :nothing
     owner 'root'
     group 'root'
-    notifies :create, 'template[/etc/elasticsearch/elasticsearch.yml]', :immediately
+    notifies :run, 'execute[install_elasticsearch]', :immediately
 end
 
 #sudo install elasticsearch from repo
 execute 'install_elasticsearch' do 
     command 'sudo yum install --enablerepo=elasticsearch elasticsearch -y'
     action :nothing
-    notifies :restart, 'execute[daemon-reload]', :immediately
-end
-
-#enable elasticsearch service to startup at boot
-execute 'daemon-reload' do
-    command 'sudo systemctl daemon-reload'
-    action :nothing
-    notifies :restart, 'service[elasticsearch.service]', :immediately
-end
-
-# elasticsearch service
-service 'elasticsearch.service' do 
-    supports :status => true, :restart => true, :reload => true
-    notifies :run, 'execute[check_elasticsearch_port_9200]', :immediately
+    notifies :create, 'template[/etc/elasticsearch/elasticsearch.yml]', :immediately
 end
 
 # update the elasticsearch configuration
 template '/etc/elasticsearch/elasticsearch.yml' do 
     source 'etc/elasticsearch/elasticsearch.yml.erb'
     action :nothing
-    notifies :run, 'execute[daemon-reload]', :immediately
+    notifies :restart, 'service[daemon-reload]', :immediately
+end
+
+#enable daemon-reload service to startup at boot
+service 'daemon-reload' do
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+    notifies :restart, 'service[elasticsearch.service]', :immediately
+end
+
+# elasticsearch service
+service 'elasticsearch.service' do 
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+    notifies :run, 'execute[check_elasticsearch_port_9200]', :immediately
 end
 
 #checks elastic is running
@@ -53,17 +57,22 @@ execute 'check_elasticsearch_port_9200' do
     action :nothing
 end
 
+
+=begin
+    @todo make more idempotency, currentlty applies even when the ports are already enabled 
+=end
+
 #enable firewalld and/or reload settings
 service 'firewalld' do 
     supports :status => true, :restart => true, :reload => true
     action [ :enable, :start ]
-    notifies :run, 'execute[daemon-reload]', :immediately
+    notifies :restart, 'service[daemon-reload]', :immediately
 end
 
 # not required as the install of elastic opens that port automatically
 execute 'allow_ports' do
-    output = Mixlib::ShellOut.new('sudo firewall-cmd --list-ports')
-    output.stdout
+    output = Mixlib::ShellOut.new('sudo firewall-cmd --list-ports').stdout
+    print(output)
     command 'firewall-cmd --permanent --add-port={9200,9300}/tcp'
     notifies :restart, 'service[firewalld]', :immediately
     # only_if { output.stdout != '9200/tcp 9300/tcp' } 
--- chef/cookbooks/logstash/recipes/logstash_build.rb
+++ chef/cookbooks/logstash/recipes/logstash_build.rb
@@ -1,6 +1,83 @@
+#return output of shell commands - Will be using ot make cookbook more idempotency (run checks if not exist/valid before execution)
+require 'mixlib/shellout'
+
 #install Java first
 package 'java-11-openjdk-devel'
 
-template '~/.bashrc' do 
-    source: 'home/bashrc.erb'
+# Updates file with JAVA_HOME Settings
+template '.bashrc' do 
+    source 'home/bashrc.erb'
+    action :create
 end
+
+=begin    
+install logstash, will run check to see if logstash 7.6 version is installed 
+before applying resources    
+=end
+
+#import PGP KEY
+execute 'download_and_install_elasticsearch_public_key' do
+    not_if "rpm -q logstash-7.6*"
+    command 'sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+    notifies :create, 'template[/etc/yum.repos.d/logstash.repo]', :immediately
+end
+
+#create repo file 
+template '/etc/yum.repos.d/logstash.repo' do 
+    source 'etc/yum.repos.d/logstash.repo.erb'
+    action :nothing
+    owner 'root'
+    group 'root'
+    notifies :run, 'execute[install_logstash]', :immediately
+end
+
+#sudo install logstash from repo that has been updated
+execute 'install_logstash' do 
+    command 'sudo yum install logstash -y'
+    action :nothing
+    notifies :create, 'template[/etc/logstash/logstash.yml]', :immediately
+end
+
+#logstash service
+service 'logstash.service' do
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+    subscribes :reload, 'template[/etc/logstash/logstash.yml]', :immediately
+end
+
+#update the logstash yml file
+template '/etc/logstash/logstash.yml' do
+    source 'etc/logstash/logstash.yml.erb'
+    action :nothing
+end
+
+
+#update the JVM options file
+template '/etc/logstash/jvm.options' do
+    source 'etc/logstash/jvm.options.erb'
+    action :create
+    notifies :restart, 'service[logstash.service]', :immediately
+end
+
+#configure logstash for filebeat input
+template '/etc/logstash/conf.d/auditbeat.conf' do
+    source 'etc/logstash/conf.d/auditbeat.conf.erb'
+    action :create
+end
+
+#enable firewalld and/or reload settings, triggers after logstash install
+service 'firewalld' do 
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+    subscribes :start, 'execute[install_logstash]', :immediately
+end
+
+# enable filebeat port
+execute 'allow_ports' do
+    output = Mixlib::ShellOut.new('sudo firewall-cmd --list-ports').stdout
+    print(output)
+    command 'firewall-cmd --permanent --add-port=5044/tcp'
+    notifies :reload, 'service[firewalld]', :immediately
+    # only_if { output.stdout != '5400/tcp' } 
+end
+
